f195524d97207909c6c67ff2bef4cb6eb182ce24
Dhruv Mohapatra <dhrvmohapatra@gmail.com>
2023-11-05 12:22:31 +0530

    revert and move server to this

--- flask/app/views.py
+++ flask/app/views.py
@@ -8,33 +8,33 @@ from .config import Config
 from .transeleos import transeleos
 
 
-@limits(calls=1, period=1)
-@app.route('/gpu_translate_to_output_lang', methods=['GET'])
+@limits(calls=1, period=10)
+@app.route('/store_translated_audio', methods=['GET'])
 def gpu_translate_to_output_lang():
-    client_ip = request.remote_addr
+    # client_ip = request.remote_addr
 
-    # TODO: uncomment for prod
-    if client_ip == Config.ALLOWED_IP:
-        # Get the video URL from the 'url' query parameter
-        video_url = request.args.get('url')
-        output_lang = request.args.get('language')
-
-        if not video_url and not output_lang:
-            return jsonify({'error': 'Missing "url" parameter'}), 400
-
-        s3_url = transeleos(video_url, output_lang)
-
-        return s3_url
-    else:
-        logging.error("invalid request")
+    # # TODO: uncomment for prod
+    # if client_ip == Config.ALLOWED_IP:
+    #     # Get the video URL from the 'url' query parameter
+    #     video_url = request.args.get('url')
+    #     output_lang = request.args.get('language')
+    #
+    #     if not video_url and not output_lang:
+    #         return jsonify({'error': 'Missing "url" parameter'}), 400
+    #
+    #     s3_url = transeleos(video_url, output_lang)
+    #
+    #     return s3_url
+    # else:
+    #     logging.error("invalid request")
 
     # Get the video URL from the 'url' query parameter
-    # video_url = request.args.get('url')
-    # output_lang = request.args.get('language')
-    #
-    # if not video_url and not output_lang:
-    #     return jsonify({'error': 'Missing "url" parameter'}), 400
-    #
-    # s3_url = transeleos(video_url, output_lang)
-    #
-    # return s3_url
+    video_url = request.args.get('url')
+    output_lang = request.args.get('language')
+
+    if not video_url and not output_lang:
+        return jsonify({'error': 'Missing "url" parameter'}), 400
+
+    s3_url = transeleos(video_url, output_lang)
+
+    return s3_url
